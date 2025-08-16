cfd082cc99bb66f1a094e67638ba7db45a274158
MennatAllah <mennamahamed3@gmail.com>
2025-08-17 00:46:50 +0200

    Apply Edits

--- app/(main)/products/page.js
+++ app/(main)/products/page.js
@@ -13,9 +13,9 @@ export default function Products() {
   return (
     <div className="secondary-bg py-50 bg-contain bg-center relative before:absolute before:w-full before:h-full before:top-0 before:left-0 before:bg-[#00000010]">
       <h2 className="text-2xl md:text-5xl opacity-50 tracking-[20px] font-bold text-[#461e04] mx-2 text-center py-3 md:py-8 uppercase">Our Products</h2>
-      <div className="grid grid-cols-6 mx-auto w-[80%] gap-6 lg:gap-10 mt-10">
+      <div className="grid grid-cols-6 mx-auto w-[80%] md:w-[90%] gap-6 lg:gap-10 mt-10 p-4">
         {prds && prds.map((prd) => (
-        <div key={prd.id} className="products-card card rounded-2xl flex flex-col items-center relative bg-[#fefcf9] shadow-lg transition-all duration-300 hover:shadow-xl hover:bg-yellow-300 col-span-3 lg:col-span-2 border-none">
+        <div key={prd.id} className="products-card card rounded-2xl flex flex-col items-center relative bg-[#fefcf9] shadow-lg transition-all duration-300 hover:shadow-xl hover:bg-yellow-300 col-span-6 md:col-span-3 lg:col-span-2 border-none">
           <Link href={`/products/${prd.id}`} className='absolute w-full h-full top-0 left-0 z-10'></Link>
           <figure className="mx-2 mt-5 overflow-hidden rounded-none">
             <img
